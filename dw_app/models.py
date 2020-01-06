a4aa837e73dbeee93aa13cda3ca3705c6ecef4dc
Regina Galambos <gina.galambos@gmail.com>
2020-01-06 04:07:16 +0100

    add history to models to track changes in data

--- dw_app/models.py
+++ dw_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from simple_history.models import HistoricalRecords
 
 class Person(models.Model):
     """ Person class is a unique physical person identified by first name, last name & email"""
@@ -8,6 +9,10 @@ class Person(models.Model):
     email = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    history = HistoricalRecords()
+
+    class Meta:
+        unique_together = (('first_name', 'last_name'),)
 
     def __str__(self):
         return '{} {}'.format(self.first_name, self.last_name)
@@ -29,6 +34,10 @@ class PersonVehicle(models.Model):
     vehicle = models.ForeignKey(Vehicle, on_delete=models.PROTECT)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
+    history = HistoricalRecords()
+
+    class Meta:
+        unique_together = (('vehicle'),)
 
     def __str__(self):
         return '{} {}'.format(self.person, self.vehicle)
--- dw_project/settings.py
+++ dw_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'simple_history',
     'dw_app',
 ]
 
@@ -48,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'simple_history.middleware.HistoryRequestMiddleware',
 ]
 
 ROOT_URLCONF = 'dw_project.urls'
