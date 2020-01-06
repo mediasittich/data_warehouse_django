4fd968d8b4dc3f64128c93102c31cd48273305d8
Regina Galambos <gina.galambos@gmail.com>
2020-01-06 05:44:57 +0100

    add api in views & define api url

--- dw_app/views.py
+++ dw_app/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import PersonVehicle
+from .serializers import PersonVehicleSerializer
+
+@api_view(['POST'])
+def person_vehicle(request):
+    """
+    View called by a POST request to add or update information
+    """
+
+    # Deserialize information provided in the request
+    ser = PersonVehicleSerializer(data=request.data)
+
+    # Validate provided information
+    ser.is_valid(raise_exception=True)
+
+    # Save information to the warehouse
+    ser.save()
+
+    return Response({'All good, everything has been saved.'})
--- dw_project/urls.py
+++ dw_project/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
+
+from dw_app.views import person_vehicle
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path('^api/personvehicle/', person_vehicle),
 ]
