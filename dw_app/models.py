83add240214776a0b0e63161a637c07ee9343ad4
Regina Galambos <gina.galambos@gmail.com>
2020-01-06 03:39:41 +0100

    create models & add app to projects settings

--- dw_app/models.py
+++ dw_app/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    """ Person class is a unique physical person identified by first name, last name & email"""
+
+    first_name = models.CharField(max_length=100) 
+    last_name = models.CharField(max_length=100) 
+    email = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return '{} {}'.format(self.first_name, self.last_name)
+
+
+class Vehicle(models.Model):
+    """ Vehicle class stores unique registration plate number"""
+    registration_plate = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return registration_plate
+
+
+class PersonVehicle(models.Model):
+    """ PersonVehicle registers a relationship between a vehicle and a person """
+    person = models.ForeignKey(Person, on_delete=models.PROTECT)
+    vehicle = models.ForeignKey(Vehicle, on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '{} {}'.format(self.person, self.vehicle)
+    
